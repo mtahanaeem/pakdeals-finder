ae457f80c0fbfd4ceab6574b507061997ba81cd6
Muhammad Tahan Azeem <mtahanaeem@users.noreply.github.com>
2026-06-09 00:30:21 +0500

    Fix load_dotenv to use absolute path to .env

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 sys.path.insert(0, os.path.dirname(__file__))
 
 from flask import Flask
